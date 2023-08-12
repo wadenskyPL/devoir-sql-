8da74cc9d1b80a250825f747d0d0898c4b954bc7
wadenskyPL <112264554+wadenskyPL@users.noreply.github.com>
2023-08-12 16:12:52 -0400

    Add files via upload

---
+++ battle1.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3307
+-- Generation Time: Aug 12, 2023 at 08:12 PM
+-- Server version: 10.6.5-MariaDB
+-- PHP Version: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `battle1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pwodwi`
+--
+
+DROP TABLE IF EXISTS `pwodwi`;
+CREATE TABLE IF NOT EXISTS `pwodwi` (
+  `name` varchar(30) NOT NULL,
+  `description` varchar(30) NOT NULL,
+  `price` decimal(10,2) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
